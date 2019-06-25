d7394f2dc58f179d1ae255d88f41c87c373f5b3d
smallMote <1746671848@qq.com>
2019-06-25 23:59:07 +0800

    vue 根组件
    删除Helloword.vue组件，新增Watcher.vue组件

--- s_1/src/App.vue
+++ s_1/src/App.vue
@@ -1,19 +1,18 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Watcher/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  import Watcher from './components/Watcher'
+  export default {
+    name: 'app',
+    components: {
+      Watcher
+    }
   }
-}
 </script>
 
 <style>
