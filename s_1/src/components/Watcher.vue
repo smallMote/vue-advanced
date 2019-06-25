6079276379285f55c349680b7265018a8668e4f8
smallMote <1746671848@qq.com>
2019-06-25 23:57:49 +0800

    深度监听器的进阶
    增加watch: {
                value: {
                    handler: 'getMoney',
                    immediate: true,
                }
            },
    配置，方便挂在完组件后就默认请求一次数据

---
+++ s_1/src/components/Watcher.vue
@@ -0,0 +1,81 @@
+<template>
+    <div id="watcher">
+        <div class="old">
+            <p>
+                我们希望渲染完成之后默认就加载数据,但不使用created钩子函数。在这里使用了
+                <span>
+                    watch: {
+                value: {
+                handler: 'getMoney',
+                immediate: true,
+                }
+                }
+                </span>
+                的结构
+            </p>
+            <label for="input"></label>
+            <input id="input" v-model="value" placeholder="You can input there for test"/>
+
+            <ul>
+                <li v-for="(m, i) in music" :key="i">$ {{ m.id }}</li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Watcher",
+        data() {
+            return {
+                value: '',
+                music: [],
+            }
+        },
+        watch: {
+            value: {
+                handler: 'getMoney',
+                immediate: true,
+            }
+        },
+        methods: {
+            getMoney() {
+                this.music = [];
+                for (let i = 0; i < 10; i++) {
+                    this.music.push(
+                        {id: Math.round(parseInt(Math.random() * 100000)).toString()}
+                    );
+                }
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    ul {
+        padding: 15px;
+        border-radius: 6px;
+        box-shadow: 0 0 5px #cccccc;
+        margin: 15px auto;
+        max-width: 300px;
+    }
+    p {
+        max-width: 300px;
+        margin: 15px auto;
+        text-align: start;
+        text-indent: 2em;
+    }
+    p span {
+        color: #409eff;
+    }
+    input{
+        min-width: 300px;
+        border: none;
+        outline: none;
+    }
+    li , input{
+        padding: 5px;
+        border-bottom: 1px dashed seagreen;
+        list-style: none;
+    }
+</style>
