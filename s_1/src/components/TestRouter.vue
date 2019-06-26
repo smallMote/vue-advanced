a122f056a47dddc1299c53d329918dbfa76b37c8
smallMote <1746671848@qq.com>
2019-06-26 21:56:45 +0800

    测试同级路由跳转created函数中函数不执行的情况
    不知道是不是因为vue-cli3.x的原因，会自动刷新数据，重新执行created中的指令

---
+++ s_1/src/components/TestRouter.vue
@@ -0,0 +1,35 @@
+<template>
+    <div id="testRouter">
+        <section>
+            <BaseButton content="页面A" @click.native="toA"></BaseButton>
+            <BaseButton content="页面B" @click.native="$router.push('/r-test/r-childB')"></BaseButton>
+        </section>
+        <section>
+            <router-view :key="$router.fullPath"></router-view>
+        </section>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "TestRouter",
+        methods: {
+            toA() {
+                this.$router.push('/r-test/r-childA');
+            }
+        }
+    }
+</script>
+
+<style scoped>
+#testRouter {
+    box-sizing: border-box;
+    transition: all 0.4s linear;
+    border: 1px solid rgba(44, 62, 35, 0.2);
+    border-radius: 6px;
+    margin: 30px;
+    padding: 15px;
+    background: #409eff;
+    color: white;
+}
+</style>
