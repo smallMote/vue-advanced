93f56b721d508995e136ee506d68557145f0033a
smallMote <1746671848@qq.com>
2019-06-26 21:51:32 +0800

    基础按钮，全局注册+自动注册

---
+++ s_1/src/components/BaseButton.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        AutoLoadComponent
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "BaseButton"
+    }
+</script>
+
+<style scoped>
+
+</style>
