43b3ec827c327da1f4c5735ee73c0e14a11147eb
smallMote <1746671848@qq.com>
2019-06-26 21:55:12 +0800

    同级路由测试组件B

---
+++ s_1/src/components/RouterChildB.vue
@@ -0,0 +1,30 @@
+<template>
+    <div id="routerChildB">
+        <p>{{msg}}</p>
+        <p>{{randomMsg}}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "RouterChildB",
+        data() {
+            return {
+                msg: 'Router Child B',
+                randomMsg: '',
+            }
+        },
+        methods: {
+            getRandomStr() {
+                this.randomMsg= Math.random().toString(32).slice(2);
+            }
+        },
+        created() {
+            this.getRandomStr();
+        }
+    }
+</script>
+
+<style scoped lang="less">
+
+</style>
