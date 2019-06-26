e9a3ebdcf3a7611987b2dd82207ea2fa2bb99f3e
smallMote <1746671848@qq.com>
2019-06-26 21:51:11 +0800

    vue根组件

--- s_1/src/App.vue
+++ s_1/src/App.vue
@@ -1,16 +1,36 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <Watcher/>
+    <router-view></router-view>
+    <MyselfComp
+      v-for="(btn, index) in btns"
+      :key="index"
+      :type="btn.type"
+      :text="btn.text"
+      class="btnGroup"
+    >
+      {{ btn.text }}
+    </MyselfComp>
   </div>
 </template>
 
 <script>
-  import Watcher from './components/Watcher'
+  import MyselfComp from './components/MyselfComp'
   export default {
     name: 'app',
     components: {
-      Watcher
+      MyselfComp
+    },
+    data() {
+      return {
+        btns: [
+          {type: 'success', text: '成功按钮'},
+          {type: 'error', text: '错误按钮'},
+          {type: 'warm', text: '警告按钮'},
+          {type: 'info', text: '信息按钮'},
+          {type: 'default', text: '默认按钮'},
+        ]
+      }
     }
   }
 </script>
@@ -24,4 +44,7 @@
   color: #2c3e50;
   margin-top: 60px;
 }
+  .btnGroup {
+    margin: 15px;
+  }
 </style>
